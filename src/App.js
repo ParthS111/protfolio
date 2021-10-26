c86e269e0edb9afe9e59bc6ac86953750d03a3a0
parth <parths@itpathsolutions.co.in>
2021-10-26 15:55:36 +0530

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
+import NavBar from './component/NavBar';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Profolioo</h1>
-    </div>
+         <NavBar/>
+     
   );
 }
 
